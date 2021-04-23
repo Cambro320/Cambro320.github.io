4ffe0984c3be4b8a474f755a27b00f1b93bd1d2c
Jory <46983170+Cambro320@users.noreply.github.com>
2021-04-23 12:52:21 -0500

    created new navbar from scratch

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const navButton = document.getElementById('menu-icon')
+const headerSocialLinks = document.querySelector('.header-social-links')
+const menu = document.querySelector('.menu')
+
+navButton.addEventListener('click', toggleActive)
+
+function toggleActive() {
+  headerSocialLinks.classList.toggle('inactive')
+  menu.classList.toggle('inactive')
+}
+
 AOS.init({
   duration: 800
 });
+
+
